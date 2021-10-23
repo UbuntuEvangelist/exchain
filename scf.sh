8396a6a9c50e81d5376fe04d7feebacb17ada7a9
chunfengSun <516108736@qq.com>
2021-10-23 19:17:33 +0800

    ddas

--- scf.sh
+++ scf.sh
@@ -9,5 +9,5 @@ exchaind init multi_run --chain-id exchain-66 --home ${EXCHAIND_PATH}
 cp /Users/oker/scf/genesis.json ${EXCHAIND_PATH}/config/genesis.json
 rm -rf ${EXCHAIND_PATH}/data
 cp -rf /Users/oker/scf/src/data ${EXCHAIND_PATH}
-#exchaind replay -d /Users/oker/scf/src/data-s1 --home ~/.exchaind   > multi_run.log 2>&1 &
-exchaind replay -d /Users/oker/scf/src/data-s1 --home ~/.exchaind --paralleled-tx    > multi_run.log 2>&1 &
+#exchaind replay -d /Users/oker/scf/src/data-s1 --home ~/.exchaind --paralleled-tx    > multi_run.log 2>&1 &
+exchaind replay -d /Users/oker/scf/src/data-s1 --home ~/.exchaind     > multi_run.log 2>&1 &
