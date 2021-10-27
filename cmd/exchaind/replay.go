a2614a402e50cedf0f1e0a45e933b437da57ad12
chunfengSun <516108736@qq.com>
2021-10-27 14:09:19 +0800

    upda

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -266,7 +266,7 @@ func doReplay(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 		blockExec.SetIsAsyncDeliverTx(viper.GetBool(sm.FlagParalleledTx))
 		state, _, err = blockExec.ApplyBlock(state, meta.BlockID, block)
 		panicError(err)
-		//SaveBlock(ctx, originBlockStore, height)
+		SaveBlock(ctx, originBlockStore, height)
 		if height >= lastBlockHeight+1000 {
 			break
 		}
